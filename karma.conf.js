9ec2d4e9e9e8ab4b22ba9837e1d1357767325d78
Johannes Stein <johannesstein@freeze-dev.com>
2015-03-25 21:09:53 +0100

    Add karma config

---
+++ karma.conf.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'chai'],
+    files: [
+      'src/**/*.js',
+      'test/**/*.js'
+    ],
+    preprocessors: {
+      'src/**/*.js': ['webpack'],
+      'test/**/*.js': ['webpack']
+    },
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          { test: /\.js$/, loader: 'babel-loader' }
+        ]
+      }
+    },
+
+    webpackServer: {
+      noInfo: true
+    },
+    
+    exclude: [],
+    port: 8080,
+    logLevel: config.LOG_INFO,
+    colors: true,
+    autoWatch: false,
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+    reporters: ['progress'],
+    captureTimeout: 60000,
+    singleRun: true
+  });
+};
