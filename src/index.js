91358646279f64e9daa6cdc0c92649aff56b9d94
Johannes Stein <johannesstein@freeze-dev.com>
2015-03-25 21:10:24 +0100

    Add animframe index file

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import requestAnimationFrame from './requestAnimationFrame';
+import cancelAnimationFrame from './cancelAnimationFrame';
+import performance from './performance';
+
+export default {requestAnimationFrame, cancelAnimationFrame, performance};
